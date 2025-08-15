26cacd0d5e1f8541ff3839674032db91168d718d
Deyson Estrada <deyson12@gmail.com>
2025-08-15 09:42:51 -0500

    Verificando

--- src/app/pages/product/cart/cart-card/cart-card.component.ts
+++ src/app/pages/product/cart/cart-card/cart-card.component.ts
@@ -66,7 +66,7 @@ export class CartCardComponent implements OnInit {
 
   role: string = '';
 
-  allowDelivery: number = 99;
+  allowDelivery: number = 99; // 0
 
   constructor(
     private readonly locationService: LocationService,
@@ -98,7 +98,7 @@ export class CartCardComponent implements OnInit {
       next: (zone) => {
         console.log('Zona de cobertura:', zone);
         this.order.deliveryPrice = zone.deliveryPrice;
-        // this.allowDelivery = zone.id !== undefined && zone.id !== null ? 1 : -1;
+        //this.allowDelivery = zone.id !== undefined && zone.id !== null ? 1 : -1;
       }
     });
 
